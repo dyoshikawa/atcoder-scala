f7a7b5c4623cd2cd77be04a8823345de9d35d936
dyoshikawa <yoshikawada@crunchtimer.jp>
2019-01-29 13:16:11 +0900

    問題追加

--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -2,19 +2,45 @@ package example
 
 import scala.io.StdIn._
 
-// ABC 086 A - Product
+// ABC 081 B - Shift Only
 object Hello extends App {
-  val a: Int = readInt()
-  val b: Int = readInt()
-  val prod = a * b
-  val even: Boolean = if (prod % 2 == 0) { true } else { false }
-  if (even) {
-    println("even")
-  } else {
-    println("odd")
+  readLine()
+  val str: String = readLine()
+  var nums: Array[Int] = str.split(" ").map(_.toInt)
+  var res: Int = 0
+
+  var loop: Boolean = true
+  while(loop) {
+    if (nums.count(_ % 2 != 0) > 0) {
+      loop = false
+    } else {
+      nums = nums.map(_ / 2)
+      res += 1
+    }
   }
+
+  println(res)
 }
 
+// ABC 081 A - Placing Marbles
+//object Hello extends App {
+//  val str: String = readLine()
+//  val chars = str.split("")
+//  println(chars.count(_ == "1"))
+//}
+
+// ABC 086 A - Product
+//object Hello extends App {
+//  val a: Int = readInt()
+//  val b: Int = readInt()
+//  val prod = a * b
+//  val even: Boolean = if (prod % 2 == 0) { true } else { false }
+//  if (even) {
+//    println("even")
+//  } else {
+//    println("odd")
+//  }
+//}
 
 //object Hello extends App {
 //  var a: Int = readInt()
